b89ce20cbbf91fd055fa63ae975fbd9281a4aac3
Elias Wachmann <elias.wachmann@gmail.com>
2024-10-11 12:46:18 +0200

    making some progress on mm product

--- 01_lab0/src/MM-product.c
+++ 01_lab0/src/MM-product.c
@@ -37,15 +37,30 @@ int checkResult(double *truth, double *test, size_t Nr_col, size_t Nr_rows) {
     return 0;
 }
 
-int productSequential(double *res) {
-    // dynamically allocate to not run into stack overflow - usually stacks are
-    // 8192 bytes big -> 1024 doubles but we have 1 Mio. per matrix
-    double(*a)[NCA] = malloc(sizeof(double) * NRA * NCA);
-    double(*b)[NCB] = malloc(sizeof(double) * NCA * NCB);
-    double(*c)[NCB] = malloc(sizeof(double) * NRA * NCB);
+struct mm_input {
+    size_t rows;
+    size_t cols;
+    double *a;  
+    double *b;  
+}MM_input;
 
-    /*** Initialize matrices ***/
+char* getbuffer(mm_input *in, size_t size_of_buffer){
+    char* buffer = (char*)malloc(size_of_buffer);
+    if (buffer == 0)
+    {
+        printf("Buffer couldn't be allocated.");
+        return NULL;
+    }
 
+    memcpy(buffer, &in.rows, sizeof(size_t));
+    memcpy(buffer + sizeof(size_t), &in.cols, sizeof(size_t));
+    memcpy(buffer + 2*sizeof(size_t), in.a, in.rows*in.cols*sizeof(double));
+    memcpy(buffer + 2*sizeof(size_t) + in.rows*in.cols*sizeof(double), in.b, in.rows*in.cols*sizeof(double));
+    return buffer;
+}
+
+
+void setupMatrices(double (*a)[NCA], double (*b)[NCB], double (*c)[NCB]){
     for (size_t i = 0; i < NRA; i++) {
         for (size_t j = 0; j < NCA; j++) {
             a[i][j] = i + j;
@@ -63,11 +78,24 @@ int productSequential(double *res) {
             c[i][j] = 0;
         }
     }
+}
+
+double productSequential(double *res) {
+    // dynamically allocate to not run into stack overflow - usually stacks are
+    // 8192 bytes big -> 1024 doubles but we have 1 Mio. per matrix
+    double(*a)[NCA] = malloc(sizeof(double) * NRA * NCA);
+    double(*b)[NCB] = malloc(sizeof(double) * NCA * NCB);
+    double(*c)[NCB] = malloc(sizeof(double) * NRA * NCB);
+
+    /*** Initialize matrices ***/
+    setupMatrices(a,b,c);
+
     /* Parallelize the computation of the following matrix-matrix
    multiplication. How to partition and distribute the initial matrices, the
    work, and collecting final results.
-*/
+    */
     // multiply
+    double start = MPI_Wtime();
     for (size_t i = 0; i < NRA; i++) {
         for (size_t j = 0; j < NCB; j++) {
             for (size_t k = 0; k < NCA; k++) {
@@ -75,7 +103,7 @@ int productSequential(double *res) {
             }
         }
     }
-
+    double time = MPI_Wtime()-start;
     /*  perform time measurement. Always check the correctness of the parallel
        results by printing a few values of c[i][j] and compare with the
        sequential output.
@@ -90,43 +118,99 @@ int productSequential(double *res) {
     free(a);
     free(b);
     free(c);
+    return time;
+}
+
+int splitwork(size_t num_workers){
+    
+    double(*a)[NCA] = malloc(sizeof(double) * NRA * NCA);
+    double(*b)[NCB] = malloc(sizeof(double) * NCA * NCB);
+    double(*c)[NCB] = malloc(sizeof(double) * NRA * NCB);
+    // Transpose matrix b to make accessing columns easier - in row major way - better cache performance
+    double (*b_transposed)[NCA] = malloc(sizeof(double) * NCA * NCB);
+    for (size_t i = 0; i < NCA; i++) {
+        for (size_t j = 0; j < NCB; j++) {
+            b_transposed[j][i] = b[i][j];
+        }
+    }
+
+    /*** Initialize matrices ***/
+    setupMatrices(a,b,c);
+    // given number of workers I'll split
+    size_t rows_per_worker = NRA / (num_workers+1); //takes corresponding columns from other matrix
+    printf("rows per worker: %zu\n", rows_per_worker);
+    size_t row_end_first = NRA - rows_per_worker*num_workers; 
+    printf("first gets most: %zu\n", row_end_first);
+
+    //setup requests
+    MPI_Request[num_workers] requests;
+    MM_input data_first; 
+    data_first.rows = row_end_first;
+    data_first.cols = row_end_first;
+    data_first.a = a; //they both start of with no offset!
+    data_first.b = b;
+    //first one
+    // MPI_Isend();
+    for (size_t i = 0; i < (num_workers-1); i++)
+    {
+        MM_input data;
+        data.rows = rows_per_worker;
+        data.cols = rows_per_worker;
+        data.a = &(a[row_end_first + rows_per_worker*i]);
+        data.b = &(b[row_end_first + rows_per_worker*i]);
+        //MPI_Isend(); // send other stuff!
+    }
+    //rest belongs to me!
+    //TODO multiply and add rest!
+    //free all pointers!
+    free(a);
+    free(b);
+    free(b_transposed);
+    free(c);
+    return 0;
+}
+
+int work(){
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    int tid, nthreads, i, j, k;
+    int tid, nthreads;
     /* for simplicity, set NRA=NCA=NCB=N  */
+    // Initialize MPI, find out MPI communicator size and process rank
+    int myRank, numProcs;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
     if (argc > 1 && strcmp(argv[1], "parallel") == 0) {
         // Variables for the process rank and number of processes
-        int myRank, numProcs, i;
-        MPI_Status status;
 
-        // Initialize MPI, find out MPI communicator size and process rank
-        MPI_Init(&argc, &argv);
-        MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
-        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
         if (myRank == 0) {
             printf("Run parallel!\n");
-            printf("Hello from master!\n");
+            printf("Hello from master! - I have %d workers!\n", numProcs-1);
+            // send out work
+            splitwork(5);
         } else {
             printf("Worker bee %d...\n", myRank);
+            work();
         }
 
-        MPI_Finalize();
     } else  // run sequantial
     {
         printf("Run sequantial!\n");
         double *res = malloc(sizeof(double) * NRA * NCB);
-        productSequential(res);
+        double time = productSequential(res);
         if (checkResult(res, res, NCB, NRA)) {
             printf("Matrices do not match!!!\n");
             return 1;
         }
-        printf("Matrices match!\n");
+        printf("Matrices match! - took: %.2f s\n", time);
         free(res);
     }
 
+    MPI_Finalize();
     return 0;
 }
