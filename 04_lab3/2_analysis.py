28917e603f8fbda29397c8ac4d347c17d785882e
Elias Wachmann <elias.wachmann@gmail.com>
2025-01-17 23:32:36 +0100

    heast es geht dem ende zu!

--- 04_lab3/2_analysis.py
+++ 04_lab3/2_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from uncertainties.unumpy import uarray
 FIG_PATH = "../lab_report/fig/lab3"
 os.chdir(os.path.dirname(__file__))
 
@@ -16,16 +17,26 @@ t_per_block_64 = np.array([[0.000436, 0.001018, 0.010970, 0.029569, 0.028081],
 t_per_block_100 = np.array([[0.000406, 0.001021, 0.006532, 0.027483, 0.043187],
                            [0.000404, 0.000880, 0.006133, 0.023168, 0.048368],
                            [0.000506, 0.000988, 0.007787, 0.025319, 0.034700]])
+t_per_block_96 = np.array([[0.000775, 0.000973, 0.008396, 0.027167, 0.057904],
+                          [0.000924, 0.001036, 0.006609, 0.043755, 0.033635],
+                          [0.000902, 0.001068, 0.010535, 0.031782, 0.035701]])
+
+vars_96 = np.var(t_per_block_96, axis=0) *1000
 
 t_per_block_32  = 1000 * np.mean(t_per_block_32, axis=0)
 t_per_block_64  = 1000 * np.mean(t_per_block_64, axis=0)
 t_per_block_100 = 1000 * np.mean(t_per_block_100, axis=0)
+t_per_block_96 = 1000 * np.mean(t_per_block_96, axis=0)
+
+t_per_block_96_u = uarray(t_per_block_96, vars_96)
+print(t_per_block_96_u)
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=14)
 
 plt.plot(Ns, t_per_block_32, label="32 threads per block")
 plt.plot(Ns, t_per_block_64, label="64 threads per block")
+plt.plot(Ns, t_per_block_96, label="96 threads per block")
 plt.plot(Ns, t_per_block_100, label="100 threads per block")
 plt.xlabel(r"N / \#")
 plt.ylabel("t / ms")
